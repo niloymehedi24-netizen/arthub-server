82f5e07ba68b5981b74481a1acc523196db00bbd
hasanniloy122-rgb <hasanniloy122@gmail.com>
2026-06-29 19:27:40 +0600

    created delete api to delete comment

--- index.js
+++ index.js
@@ -437,6 +437,16 @@ async function run() {
     }
     });
 
+    app.delete("/api/comments/:id", async (req, res) => {
+    try {
+    const result = await commentsCollection.deleteOne({_id: new ObjectId(req.params.id)});
+
+    res.send(result);
+    } catch (err) {
+    res.status(500).send({ message: "Delete failed" });
+    }
+    });
+
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
